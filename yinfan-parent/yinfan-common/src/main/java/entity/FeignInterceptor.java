f20b36297e2e6466ec09f63906fb3572f6c74ffc
79412906@qq.com <79412906zhang>
2020-04-01 09:07:22 +0800

    项目基础搭建

---
+++ yinfan-parent/yinfan-common/src/main/java/entity/FeignInterceptor.java
@@ -0,0 +1,45 @@
+package entity;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Enumeration;
+
+
+public class FeignInterceptor implements RequestInterceptor {
+
+    /**
+     * feign执行之前拦截
+     * @param requestTemplate
+     */
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        try {
+            //使用RequestContextHolder工具获取request相关变量
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            if (attributes != null) {
+                //取出request
+                HttpServletRequest request = attributes.getRequest();
+                //获取所有头文件信息的key
+                Enumeration<String> headerNames = request.getHeaderNames();
+                if (headerNames != null) {
+                    while (headerNames.hasMoreElements()) {
+                        //头文件的key
+                        String name = headerNames.nextElement();
+                        //头文件的value
+                        String values = request.getHeader(name);
+                        System.out.println("头信息=========="+ name+ ":" + values );
+                        //将令牌数据添加到头文件中
+                        requestTemplate.header(name, values);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ yinfan-parent/yinfan-eureka/src/main/java/com/yinfan/EurekaApplication.java
@@ -0,0 +1,18 @@
+package com.yinfan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    /**
+     * 加载启动类
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
